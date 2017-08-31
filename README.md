dee3a0a3fda9a515bb97f65940fe81df868d4619
mongkol.me <mongkol.me@glsict.com>
2017-08-31 11:49:12 +0700

    graphql with data.json file

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # GraphQL_CustomerBase
 
+# install package
+
+    npm init
+    
+    npm install express express-graphql nodemon --save
+
+    npm run dev:server
+
+    npm install graphql --save
+
+    npm install json-server axios --save
+
+    npm run json:server
+
 Thanks Traversy Media
--- schema.js
+++ schema.js
@@ -1,3 +1,4 @@
+const axios = require('axios');
 const{
     GraphQLObjectType,
     GraphQLString,
@@ -18,12 +19,14 @@ const CustomerType = new GraphQLObjectType({
     })
 });
 
+/*
 // Hardcoded data
 const customers = [
     {id:'1', name:'John Doe', email:'jdoe@gmail.com', age:35},
     {id:'2', name:'Steve Smith', email:'steve@gmail.com', age:25},
     {id:'3', name:'Sara Williams', email:'sara@gmail.com', age:32},
 ];
+*/
 
 // Root Query
 const RootQuery = new GraphQLObjectType({
@@ -35,17 +38,22 @@ const RootQuery = new GraphQLObjectType({
                 id:{type:GraphQLString}
             },
             resolve(parentValue, args){
+                /*
                 for(let i = 0; i< customers.length; i++){
                     if(customers[i].id == args.id){
                         return customers[i];
                     }
                 }
+                */http://localhost:3000/customers
+                return axios.get('http://localhost:3000/customers/' + args.id)
+                    .then(res => res.data);
             }
         },
         customers:{
             type: new GraphQLList(CustomerType),
             resolve(parentValue, args){
-                return customers;
+                return axios.get('http://localhost:3000/customers')
+                .then(res => res.data);
             }
         }
     }
